08eb9459bae040b1a2863aa5d1732bb6a8016622
wangrui <wrmistra@gmail.com>
2020-05-31 20:55:34 +0800

    Initial commit

---
+++ src/main/java/com/mistra/jmt/MistraJMTApplication.java
@@ -0,0 +1,21 @@
+package com.mistra.jmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2020/5/31 20:50
+ * @ Description: JVM monitoring and tuning
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+@SpringBootApplication
+public class MistraJMTApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MistraJMTApplication.class, args);
+    }
+}
