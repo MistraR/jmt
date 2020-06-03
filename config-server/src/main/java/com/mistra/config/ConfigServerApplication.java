9778b455d6c162d089b4980521b5dab748250332
wangrui <wrmistra@gmail.com>
2020-06-03 21:00:57 +0800

    ConfigServer

---
+++ config-server/src/main/java/com/mistra/config/ConfigServerApplication.java
@@ -0,0 +1,21 @@
+package com.mistra.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2020/6/3 20:54
+ * @ Description:
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
---
+++ service-center/src/main/java/com/mistra/center/ServiceCenterApplication.java
@@ -0,0 +1,21 @@
+package com.mistra.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2020/6/3 20:55
+ * @ Description:
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+@SpringBootApplication
+public class ServiceCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceCenterApplication.class, args);
+    }
+}
