d4d782d7a9d2cfea07b8025f095a7ad6af4b2833
wangrui <wrmistra@gmail.com>
2020-06-04 21:46:43 +0800

    TourMemory

---
+++ jmt-core/src/main/java/com/mistra/jmt/core/TourMemory.java
@@ -0,0 +1,95 @@
+package com.mistra.jmt.core;
+
+import com.mistra.jmt.dao.ThreadPoolMemoryDumpRepository;
+import com.mistra.jmt.model.ThreadPoolMemoryDump;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2020/6/3 21:54
+ * @ Description: 计算各个监控对象的内存占用情况
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+@Slf4j
+@Component
+public class TourMemory {
+
+    @Autowired
+    private ThreadPoolMemoryDumpRepository threadPoolMemoryDumpRepository;
+
+    @Scheduled(fixedDelay = 300000)
+    private void tour() {
+        tourThreadPools();
+    }
+
+    /**
+     * 监控线程池
+     */
+    private void tourThreadPools() {
+        try {
+            log.info("");
+            List<ThreadPoolMemoryDump> threadPoolMemoryDumpList = new ArrayList<>();
+            for (String key : ThreadPoolWarden.getThreadPoolKeeper().keySet()) {
+                ThreadPoolExecutor threadPoolExecutor = ThreadPoolWarden.getThreadPoolKeeper().get(key);
+                if (!threadPoolExecutor.isTerminated()) {
+                    threadPoolMemoryDumpList.add(tourThreadPool(threadPoolExecutor));
+                } else {
+                    threadPoolMemoryDumpList.add(ThreadPoolMemoryDump.builder()
+                            .status("Stopped")
+                            .build());
+                    log.info("The thread pool is stopped!");
+                }
+            }
+            if (!threadPoolMemoryDumpList.isEmpty()) {
+                threadPoolMemoryDumpRepository.saveAll(threadPoolMemoryDumpList);
+            }
+        } catch (IllegalAccessException illegalAccessException) {
+
+        } catch (Exception exception) {
+
+        }
+    }
+
+    /**
+     * 统计线程池快照指标
+     *
+     * @param threadPoolExecutor 线程池
+     * @return 统计信息
+     */
+    private ThreadPoolMemoryDump tourThreadPool(ThreadPoolExecutor threadPoolExecutor) throws IllegalAccessException {
+        return ThreadPoolMemoryDump.builder()
+                .corePoolSize(threadPoolExecutor.getCorePoolSize())
+                .queueSize(threadPoolExecutor.getQueue().size())
+                .queueMemorySize(SizeOfObjectUtil.fullSizeOf(threadPoolExecutor.getQueue()))
+                .build();
+    }
+
+    /**
+     * 获取某个线程池当前的指标信息
+     *
+     * @param threadPoolName 线程池名称
+     * @return ThreadPoolMemoryDump
+     * @throws IllegalAccessException
+     */
+    private ThreadPoolMemoryDump getThreadPoolSnapshot(String threadPoolName) throws IllegalAccessException {
+        if (ThreadPoolWarden.getThreadPoolKeeper().containsKey(threadPoolName)) {
+            ThreadPoolExecutor threadPoolExecutor = ThreadPoolWarden.getThreadPoolKeeper().get(threadPoolName);
+            if (!threadPoolExecutor.isTerminated()) {
+                return tourThreadPool(threadPoolExecutor);
+            } else {
+                return ThreadPoolMemoryDump.builder().status("Stopped").build();
+            }
+        }
+        return ThreadPoolMemoryDump.builder().status("Not existed").build();
+    }
+}
